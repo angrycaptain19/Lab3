f8277dc94709d2fc3775e1531bf1fa2330fad342
dovyd <dovydas0000@gmail.com>
2021-02-28 18:03:45 +0200

    Trecias uzduotis

--- main.py
+++ main.py
@@ -12,3 +12,23 @@ def antras():
     print(sorted_list)
     print(sorted_list[::-1])
 antras()
+
+"""
+Perform list element transformation: 
+1. by using formula: x = x * (x - 10), 2. square each element.
+ my_list = [12, 45, 23, 56, -546, 34]
+"""
+def trecias():
+    my_list = [12, 45, 23, 56, -546, 34]
+    first = []
+    second = []
+    for i in range(len(my_list)):
+        print(my_list[i])
+        x = my_list[i] * (my_list[i] - 10)
+        y = my_list[i] * my_list[i]
+        first.append(x)
+        second.append(y)
+    print(first)
+    print(second)
+
+trecias()
