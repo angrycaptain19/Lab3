716a52cdc95f0a5d00415038028649a1d25dd950
dovyd <dovydas0000@gmail.com>
2021-02-28 17:49:39 +0200

    Antra uzduotis

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+from math import sqrt
+"""
+Sort the list by item length in ascending and descending order.
+ sort_me = ["Kaunas", "Vilnius", 
+ "Alytus", "Klaipeda", "Varena", "Druskininkai", "Klaipeda"]
+"""
+def antras():
+    sort_me = ["Kaunas", "Vilnius",
+               "Alytus", "Klaipeda", "Varena", "Druskininkai", "Klaipeda"]
+    sorted_list = sorted(sort_me, key=len)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(sorted_list)
+    print(sorted_list[::-1])
+antras()
